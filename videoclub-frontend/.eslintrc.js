97f34d63cfde74fb613670adb79c6306fa08fea5
Juan José González Abril <juanjo@gonzabril.es>
2019-12-01 19:14:03 +0100

    Updated code style settings

--- videoclub-frontend/.eslintrc.js
+++ videoclub-frontend/.eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     extends: ["plugin:vue/recommended", "@vue/typescript"],
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "vue/html-closing-bracket-newline": ["warn", {"multiline": "never"}],
     },
     parserOptions: {
         parser: "@typescript-eslint/parser"
